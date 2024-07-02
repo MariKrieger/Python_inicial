046ce7cd23ec35db909b4a9cf3a2f7fe0d357b6f
MariKrieger <138412881+MariKrieger@users.noreply.github.com>
2024-07-02 15:24:51 -0300

    Extras de python
    Atividades extras de Python

---
+++ Extras/hipotenusa.py
@@ -0,0 +1,5 @@
+c1 = int(input('Digite o valor do cateto um: '))
+c2 = int(input('Digite o valor do cateto dois: '))
+hipotenusa_ao_quadrado = (c1**2)+ (c2**2)
+hipotenusa = hipotenusa_ao_quadrado**0.5
+print(f"A hipotenusa desse triângulo retângulo é de: {hipotenusa}")
