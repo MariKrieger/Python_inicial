60ff8a175e81902186d277fd7a1a665ea9019973
MariKrieger <138412881+MariKrieger@users.noreply.github.com>
2024-07-10 13:55:00 -0300

    Prova
    Estudos para a prova

---
+++ prova/pré_prova.py
@@ -0,0 +1,6 @@
+import math
+
+numero = float(input("Digite um número com vírgula: "))
+parte_inteira = math.trunc(numero)
+print("A parte inteira do número {} é {}".format(numero, parte_inteira))
+
